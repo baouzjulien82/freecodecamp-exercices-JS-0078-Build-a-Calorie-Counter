c309cdda34f4f4dc741fafb787eb159daa5d6b08
Julien Baouz <baouzjulien@gmail.com>
2025-11-18 21:57:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,10 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
-  const regex = /hello/;
+  const regex = /[+-\s]/g;
+  return str.replace(regex, '');
+}
+
+function isInvalidInput(str) {
+
 }
